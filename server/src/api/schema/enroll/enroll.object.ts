a9d488a48376fccf6e8d3a2993bde9c45ab2a762
raminjoshua13 <raminjoshua05@gmail.com>
2024-03-29 07:56:50 +0800

    fixed

--- server/src/api/schema/enroll/enroll.object.ts
+++ server/src/api/schema/enroll/enroll.object.ts
@@ -1,6 +1,5 @@
 import { objectType } from "nexus";
-
-
+import { prisma } from "../../helpers/services";
 
 export const EnrollObject = objectType({
     name: "enroll",
@@ -9,5 +8,33 @@ export const EnrollObject = objectType({
         t.string("status");
         t.datetime("createdAt");
         t.datetime("updatedAt");
+        t.field("subject", {
+            type: "subject",
+            resolve: async ({ enrollID }): Promise<any> => {
+                return await prisma.subject.findMany({
+                    where: {
+                        Enroll: {
+                            some: {
+                                enrollID
+                            }
+                        }
+                    }
+                })
+            }
+        })
+        t.field("user", {
+            type: "user",
+            resolve: async ({ enrollID }): Promise<any> => {
+                return await prisma.user.findMany({
+                    where: {
+                        Enroll: {
+                            some: {
+                                enrollID
+                            }
+                        }
+                    }
+                })
+            }
+        })
     }
 })
