b7a8d4bc25cf9cf0eaaf479a9653e42462b1803c
raminjoshua13 <raminjoshua05@gmail.com>
2024-04-01 03:49:38 +0800

    added login functionalitist and working on register page

---
+++ client/src/auth/register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Register() {
+   return (
+      <div>
+         <form>
+            <button>Next</button>
+            <button>Prev</button>
+            <button>Submit</button>
+         </form>
+      </div>
+   );
+}
